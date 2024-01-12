3a03048d8b7738cc4801eef32bd53dbf066c894e
Maaz Munir <maazmunir99@gmail.com>
2024-01-12 23:34:58 +0500

    oxylabs/common.go,utils.go -> internal/utils.go

--- internal/utils.go
+++ internal/utils.go
@@ -1,17 +1,22 @@
-package oxylabs
+package internal
 
 import (
 	"fmt"
 	"net/url"
 	"strings"
-	"time"
 )
 
-var (
-	DefaultTimeout      = 50 * time.Second
-	DefaultPollInterval = 2 * time.Second
-)
+// Checks if the parameter is in the list of accepted parameters.
+func InList[T comparable](val T, list []T) bool {
+	for _, item := range list {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
 
+// Checks if the provided url is valid.
 func ValidateURL(
 	inputURL string,
 	host string,
