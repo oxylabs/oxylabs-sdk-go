c5e9503bba8b6fed9bab59d7a34245dd85c6d8c0
Maaz Munir <maazmunir99@gmail.com>
2024-01-13 18:14:43 +0500

    serp/resp.go -> internal/resp.go

--- internal/resp.go
+++ internal/resp.go
@@ -1,4 +1,4 @@
-package serp
+package internal
 
 import (
 	"encoding/json"
@@ -114,13 +114,6 @@ type Response struct {
 	Status            string   `json:"status"`
 }
 
-type ResponseProxy struct {
-	ContentParsed Content
-	Content       string
-	StatusCode    int
-	Status        string
-}
-
 type Job struct {
 	CallbackURL         string        `json:"callback_url"`
 	ClientID            int           `json:"client_id"`
@@ -187,15 +180,28 @@ type Content struct {
 }
 
 type Results struct {
-	Pla               Pla               `json:"pla"`
-	Paid              []Paid            `json:"paid"`
-	Organic           []Organic         `json:"organic"`
-	Knowledge         Knowledge         `json:"knowledge"`
-	LocalPack         LocalPack         `json:"local_pack"`
-	InstantAnswers    []InstantAnswer   `json:"instant_answers"`
-	RelatedSearches   RelatedSearches   `json:"related_searches"`
-	SearchInformation SearchInformation `json:"search_information"`
-	TotalResultsCount int               `json:"total_results_count"`
+	Pla                        Pla                          `json:"pla"`
+	Paid                       []Paid                       `json:"paid"`
+	Images                     Image                        `json:"images"`
+	Organic                    []Organic                    `json:"organic"`
+	Twitter                    []Twitter                    `json:"twitter"`
+	Knowledge                  Knowledge                    `json:"knowledge"`
+	LocalPack                  LocalPack                    `json:"local_pack"`
+	TopStories                 TopStory                     `json:"top_stories"`
+	PopularProducts            PopularProducts              `json:"popular_products"`
+	RelatedSearches            RelatedSearches              `json:"related_searches"`
+	RelatedQuestions           RelatedQuestions             `json:"related_questions"`
+	SearchInformation          SearchInformation            `json:"search_information"`
+	ItemCarousel               ItemCarousel                 `json:"item_carousel"`
+	Recipes                    Recipes                      `json:"recipes"`
+	Videos                     Videos                       `json:"videos"`
+	FeaturedSnippet            []FeaturedSnippet            `json:"featured_snippet"`
+	RelatedSearchesCategorized []RelatedSearchesCategorized `json:"related_searches_categorized"`
+	Hotels                     Hotels                       `json:"hotels"`
+	Flights                    Flights                      `json:"flights"`
+	VideoBox                   VideoBox                     `json:"video_box"`
+	LocalServiceAds            LocalServiceAds              `json:"local_service_ads"`
+	TotalResultsCount          int                          `json:"total_results_count"`
 }
 
 type Pla struct {
@@ -213,10 +219,75 @@ type PlaItem struct {
 	ImageData string `json:"image_data"`
 }
 
-type InstantAnswer struct {
-	Type       string `json:"type"`
-	Parsed     bool   `json:"_parsed"`
-	PosOverall int    `json:"pos_overall"`
+type Paid struct {
+	Pos        int           `json:"pos"`
+	URL        string        `json:"url"`
+	Desc       string        `json:"desc"`
+	Title      string        `json:"title"`
+	DataRw     string        `json:"data_rw"`
+	DataPcu    []string      `json:"data_pcu"`
+	Sitelinks  PaidSitelinks `json:"sitelinks"`
+	URLShown   string        `json:"url_shown"`
+	PosOverall int           `json:"pos_overall"`
+}
+
+type PaidSitelinks struct {
+	Expanded []Expanded `json:"expanded,omitempty"`
+	Inline   []Inline   `json:"inline,omitempty"`
+}
+
+type Expanded struct {
+	URL   string `json:"url"`
+	Desc  string `json:"desc"`
+	Title string `json:"title"`
+}
+
+type Inline struct {
+	URL   string `json:"url"`
+	Desc  string `json:"desc"`
+	Title string `json:"title"`
+}
+
+type Image struct {
+	Items      []ImageItem `json:"items"`
+	PosOverall int         `json:"pos_overall"`
+}
+
+type ImageItem struct {
+	Alt string `json:"alt"`
+	Pos int    `json:"pos"`
+	URL string `json:"url"`
+}
+
+type Organic struct {
+	Pos        int              `json:"pos"`
+	URL        string           `json:"url"`
+	Desc       string           `json:"desc"`
+	Title      string           `json:"title"`
+	Images     []string         `json:"images"`
+	Sitelinks  OrganicSitelinks `json:"sitelinks,omitempty"`
+	URLShown   string           `json:"url_shown"`
+	PosOverall int              `json:"pos_overall"`
+}
+
+type OrganicSitelinks struct {
+	Expanded []Expanded `json:"expanded,omitempty"`
+	Inline   []Inline   `json:"inline,omitempty"`
+}
+
+type Twitter struct {
+	Pos        int           `json:"pos"`
+	URL        string        `json:"url"`
+	Items      []TwitterItem `json:"items"`
+	Title      string        `json:"title"`
+	PosOverall int           `json:"pos_overall"`
+}
+
+type TwitterItem struct {
+	Pos       int    `json:"pos"`
+	URL       string `json:"url"`
+	Content   string `json:"content"`
+	TimeFrame string `json:"time_frame"`
 }
 
 type Knowledge struct {
@@ -252,56 +323,43 @@ type RelatedSearch struct {
 }
 
 type LocalPack struct {
-	Items      []Item `json:"items"`
-	PosOverall int    `json:"pos_overall"`
+	Items      []LocalPackItem `json:"items"`
+	PosOverall int             `json:"pos_overall"`
 }
 
-type Item struct {
-	Cid         string        `json:"cid"`
-	Pos         int           `json:"pos"`
-	Links       []LinkElement `json:"links"`
-	Title       string        `json:"title"`
-	Rating      int           `json:"rating"`
-	Address     string        `json:"address"`
-	RatingCount int           `json:"rating_count"`
+type LocalPackItem struct {
+	Cid         string `json:"cid"`
+	Pos         int    `json:"pos"`
+	Title       string `json:"title"`
+	Rating      int    `json:"rating"`
+	Address     string `json:"address"`
+	Subtitle    string `json:"subtitle"`
+	RatingCount int    `json:"rating_count"`
 }
 
-type Organic struct {
-	Pos        int              `json:"pos"`
-	URL        string           `json:"url"`
-	Desc       string           `json:"desc"`
-	Title      string           `json:"title"`
-	Sitelinks  OrganicSitelinks `json:"sitelinks,omitempty"`
-	URLShown   string           `json:"url_shown"`
-	PosOverall int              `json:"pos_overall"`
+type TopStory struct {
+	Items      []TopStoryItem `json:"items"`
+	PosOverall int            `json:"pos_overall"`
 }
 
-type OrganicSitelinks struct {
-	Expanded []Profile `json:"expanded,omitempty"`
-	Inline   []Profile `json:"inline,omitempty"`
+type TopStoryItem struct {
+	Pos       int    `json:"pos"`
+	URL       string `json:"url"`
+	Title     string `json:"title"`
+	Source    string `json:"source"`
+	TimeFrame string `json:"time_frame"`
 }
 
-type Paid struct {
-	Pos        int           `json:"pos"`
-	URL        string        `json:"url"`
-	Desc       string        `json:"desc"`
-	Title      string        `json:"title"`
-	DataRw     string        `json:"data_rw"`
-	DataPcu    []string      `json:"data_pcu"`
-	Sitelinks  PaidSitelinks `json:"sitelinks,omitempty"`
-	URLShown   string        `json:"url_shown"`
-	PosOverall int           `json:"pos_overall"`
+type PopularProducts struct {
+	Items      []PopularProductsItem `json:"items"`
+	PosOverall int                   `json:"pos_overall"`
 }
 
-type PaidSitelinks struct {
-	Expanded []Expanded `json:"expanded,omitempty"`
-	Inline   []Profile  `json:"inline,omitempty"`
-}
-
-type Expanded struct {
-	URL   string `json:"url"`
-	Desc  string `json:"desc"`
-	Title string `json:"title"`
+type PopularProductsItem struct {
+	Pos       int    `json:"pos"`
+	Price     string `json:"price"`
+	Title     string `json:"title"`
+	ImageData string `json:"image_data"`
 }
 
 type RelatedSearches struct {
@@ -309,8 +367,135 @@ type RelatedSearches struct {
 	RelatedSearches []string `json:"related_searches"`
 }
 
+type RelatedQuestions struct {
+	Items      []RelatedQuestionsItem `json:"items"`
+	PosOverall int                    `json:"pos_overall"`
+}
+
+type RelatedQuestionsItem struct {
+	Pos      int    `json:"pos"`
+	Answer   string `json:"answer"`
+	Source   Source `json:"source"`
+	Question string `json:"question"`
+}
+
+type Source struct {
+	URL      string `json:"url"`
+	Title    string `json:"title"`
+	UrlShown string `json:"url_shown"`
+}
+
 type SearchInformation struct {
 	Query             string `json:"query"`
 	ShowingResultsFor string `json:"showing_results_for"`
 	TotalResultsCount int    `json:"total_results_count"`
+}
+
+type ItemCarousel struct {
+	Items      []ItemCarouselItem `json:"items"`
+	Title      string             `json:"title"`
+	PosOverall int                `json:"pos_overall"`
+}
+
+type ItemCarouselItem struct {
+	Pos      int    `json:"pos"`
+	Href     string `json:"href"`
+	Title    string `json:"title"`
+	Subtitle string `json:"subtitle"`
+}
+
+type Recipes struct {
+	Items      []RecipesItem `json:"items"`
+	PosOverall int           `json:"pos_overall"`
+}
+
+type RecipesItem struct {
+	Pos      int    `json:"pos"`
+	URL      string `json:"url"`
+	Desc     string `json:"desc"`
+	Title    string `json:"title"`
+	Rating   int    `json:"rating"`
+	Source   string `json:"source"`
+	Duration string `json:"duration"`
+}
+
+type Videos struct {
+	Items      []VideosItem `json:"items"`
+	PosOverall int          `json:"pos_overall"`
+}
+
+type VideosItem struct {
+	Pos    int    `json:"pos"`
+	URL    string `json:"url"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Source string `json:"source"`
+}
+
+type FeaturedSnippet struct {
+	URl        string `json:"url"`
+	Desc       string `json:"desc"`
+	Title      string `json:"title"`
+	URLShown   string `json:"url_shown"`
+	PosOverall int    `json:"pos_overall"`
+}
+
+type RelatedSearchesCategorized struct {
+	Items      []RelatedSearchesCategorizedItem `json:"items"`
+	Category   Category                         `json:"category"`
+	PosOverall int                              `json:"pos_overall"`
+}
+
+type RelatedSearchesCategorizedItem struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
+
+type Category struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Hotels struct {
+	DateTo  string `json:"date_to"`
+	Results []struct {
+		Price       string `json:"price"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	} `json:"results"`
+	DateFrom   string `json:"date_from"`
+	PosOverall int    `json:"pos_overall"`
+}
+
+type Flights struct {
+	To      string `json:"to"`
+	From    string `json:"from"`
+	DateTo  string `json:"date_to"`
+	Results []struct {
+		URL      string `json:"url"`
+		Type     string `json:"type"`
+		Price    string `json:"price"`
+		Airline  string `json:"airline"`
+		Duration string `json:"duration"`
+	} `json:"results"`
+	DateFrom   string `json:"date_from"`
+	PosOverall int    `json:"pos_overall"`
+}
+
+type VideoBox struct {
+	URL        string `json:"url"`
+	Title      string `json:"title"`
+	PosOverall int    `json:"pos_overall"`
+}
+
+type LocalServiceAds struct {
+	Items []struct {
+		Pos              int    `json:"pos"`
+		URL              string `json:"url"`
+		Title            string `json:"title"`
+		Rating           int    `json:"rating"`
+		ReviewsCount     int    `json:"reviews_count"`
+		GoogleGuaranteed bool   `json:"google_guaranteed"`
+	} `json:"items"`
+	PosOverall int `json:"pos_overall"`
 }
